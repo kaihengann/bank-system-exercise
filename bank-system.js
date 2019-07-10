d8b891f023b42b661ff3455ed53cac9754da0c00
n4t0b0t <natalieong1990@users.noreply.github.com>
2019-07-10 17:02:39 +0800

    adds deposit test

--- bank-system.js
+++ bank-system.js
@@ -1,1 +1,7 @@
-cla
+class BankAccount {
+  constructor() {
+    this.balance = 0;
+  }
+}
+
+module.exports = BankAccount;
--- bank-system.test.js
+++ bank-system.test.js
@@ -1,6 +1,14 @@
-describe('BankAccount', () => {
-  test('bankAccount1 should be an instance of BankAccount class', () => {
-    const bankAccount1 = new BankAccount(0)
+const BankAccount = require("./bank-system.js");
+
+describe("BankAccount", () => {
+  const bankAccount1 = new BankAccount();
+
+  test("bankAccount1 should be an instance of BankAccount class", () => {
     expect(bankAccount1.balance).toEqual(0);
+  });
+
+  test("bankAccount1 should have balance of 5 after deposit of 5", () => {
+    bankAccount1.deposit(5);
+    expect(bankAccount1.balance).toEqual(5);
   });
 });
