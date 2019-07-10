7eff7b274cb6890feab57aac41d8aa9eb97119d8
kaihengann <annkaiheng@gmail.com>
2019-07-10 16:52:12 +0800

    Add test

--- bank-system.test.js
+++ bank-system.test.js
@@ -1,6 +1,6 @@
 describe('BankAccount', () => {
   test('bankAccount1 should be an instance of BankAccount class', () => {
-    const bankAccount1 = new BankAccount()
-    expect(bankAccount1 instanceof BankAccount).toEqual(true);
+    const bankAccount1 = new BankAccount(0)
+    expect(bankAccount1.balance).toEqual(0);
   });
 });
